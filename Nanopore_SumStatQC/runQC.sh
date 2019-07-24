72d9effcc0de72476e718db0341f260869955acd
stephane plaisance <stephane.plaisance@vib.be>
2019-07-24 11:01:52 +0200

    edits

--- Nanopore_SumStatQC/runQC.sh
+++ Nanopore_SumStatQC/runQC.sh
@@ -12,12 +12,12 @@ mkdir -p ${working}/RawData
 echo "# processing $1"
 echo "# flow-cell.ID: $2"
 infile="${working}/RawData/$(basename $1).bz2"
-bgzip -c $1 > ${infile}
+#bgzip -c $1 > ${infile}
 
 if [[ ! -z "$3" ]]; then
 echo "# processing barcodes from $3"
 bcfile="${working}/RawData/$(basename $3).bz2"
-bgzip -c $3 > ${bcfile}
+#bgzip -c $3 > ${bcfile}
 else
 bcfile=""
 fi
@@ -36,4 +36,5 @@ sampleIntervalMinutes: 60" | tee config.yaml
 
 cp config.yaml $(dirname $0)/
 
-R --slave -e "rmarkdown::render(\"${script_base}/${script}\", \"html_document\")"
+echo "# edit $(dirname $0)/config.yaml"
+echo "# then run with: R --slave -e 'rmarkdown::render(\"${script_base}/${script}\", \"html_document\")' "
